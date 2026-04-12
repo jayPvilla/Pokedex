e580b3f5a435e033bd11a1a9b177dd3738d5fa28
JP <jaypvilla021@gmail.com>
2026-04-12 21:15:07 +0800

    Set the fetch limit to 1000 pokemons

--- src/api/pokemon_api.js
+++ src/api/pokemon_api.js
@@ -2,7 +2,7 @@ export const base_url = 'https://pokeapi.co/api/v2/pokemon';
 
 export const getPokemons = async () => {
     try {
-        const response = await fetch(base_url)
+        const response = await fetch(`${base_url}?limit=1000`)
         const data = await response.json();
         console.log(data);
         return data.results;
