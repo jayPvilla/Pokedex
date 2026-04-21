bd8d46fc0db8ca31f35e8ceaf784a23792158268
JohnPaul <johnpaul@vourvon.com>
2026-04-21 09:43:54 +0800

    Style the navigation bar (change orientation from vertical to horizontal

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import '../css/Navbar.css'
 import { Link } from 'react-router-dom'
+import pokedexIcon from '../assets/pokedex_icon.png'
 
 const NavBar = () => {
   return (
     <nav className='navBar'>
+      <div className='title-container'>
+        <img src={pokedexIcon} alt="Pokedex Icon" className="navbar-icon" />
+        <h1 className='app-title'>POKEDEX</h1>
+      </div>
+      <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/your_pokemon">Your Pokemon</Link></li>
+        <li><Link to="/your_pokemon">My Pokemon</Link></li>
         <li><Link to="/about">About</Link></li>
+      </ul>
     </nav>
   )
 }
